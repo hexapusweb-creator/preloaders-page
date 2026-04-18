b57051294b0c8311dfec3c65b5938e55f0e807d7
Maicon Cabral <44178051+MaiconCabral@users.noreply.github.com>
2026-04-18 19:43:05 -0300

    publishing first pre-loading effect

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/styles/main.scss'
+
+function handleDisablePreloader(selector, time){
+ setTimeout(() =>{
+  const element = document.querySelector(selector);
+  if(element){
+   element.style.display = 'none';
+  }
+ }, time);
+}
+
+// handleDisablePreloader('.preloader', 5000);
